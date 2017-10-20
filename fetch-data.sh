e824f4c4784eef4785d08d051ed98a0754edc7c2
russellgoldenberg <russellgoldenberg@gmail.com>
2017-10-20 17:10:19 -0400

    old

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/env bash
 
 function getRedditToken() {
-	local PASS=$(< password)
+	local PASSWORD=$(< password)
+
 	echo "getting reddit token..."
+
 	curl \
 	-X POST \
 	-A "Terminal/0.1 by codenberg" \
-	-d 'grant_type=password&username=codenberg&password=$PASSWORD' \
+	-d "grant_type=password&username=codenberg&password=$PASSWORD" \
 	--user 'ak97h9vHuptjZw:ymq7XDpVzOT4ytMzNSejASAeEq8' \
 	https://www.reddit.com/api/v1/access_token \
 	| jq '.access_token' \
@@ -15,10 +17,12 @@ function getRedditToken() {
 }
 
 function queryRedditAPI() {
-	local TOKEN=$(< output/token)
+	local TOKEN=$(< token)
 	local TERM="$1"
 	local DATE=$(date -u +"%Y-%m-%d")
+
 	echo "querying reddit for $TERM..."
+
 	curl \
 	-s \
 	-H "Authorization: bearer $TOKEN" \
@@ -27,14 +31,19 @@ function queryRedditAPI() {
 	| jq '[.data.children[]
 		| .data
 		| { id, title, score, url, created_utc, num_comments, media_title: .media.oembed.title, media_thumbnail_width: .media.oembed.thumbnail_width, media_thumbnail_height: .media.oembed.thumbnail_height, media_thumbnail_url: .media.oembed.thumbnail_url, media_width: .media.oembed.width, media_height: .media.oembed.height }
-		| select(.score >= 1000 )]' \
+		| select(.score >= 1000 )
+		]' \
 	| in2csv -f json > output/query/$DATE--$TERM.csv
 }
 
 function concatResults() {
 	echo "concatenating all query result files..."
-	csvstack output/query/*.csv > output/query--all.csv
+
+	csvstack output/query/*.csv \
+	| csvgrep -c "url" -r "streamable|gfycat" \
+	> output/query--all.csv
 }
+
 
 # mute stderr for prettiness
 exec 2>/dev/null
